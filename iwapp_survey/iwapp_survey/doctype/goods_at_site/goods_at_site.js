f22de67f45ec865e2e38352c45ebdad0a37a67b7
Shaikh Aazar <shaikhaazar11@gmail.com>
2024-06-12 17:59:14 +0530

    update

--- iwapp_survey/iwapp_survey/doctype/goods_at_site/goods_at_site.js
+++ iwapp_survey/iwapp_survey/doctype/goods_at_site/goods_at_site.js
@@ -1,58 +1,53 @@
-// Copyright (c) 2024, Iwex Informatics and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Goods at Site', {
 	refresh: function (frm) {
 		frm.trigger("update_indicators");
+
 		// Ensure the custom buttons are only added once
 		if (!frm.custom_buttons_added) {
 			// Add the first button (Return Draft)
 			frm.add_custom_button(__("Return Draft"), function () {
 				frappe.call({
 					method: "iwapp_survey.iwapp_survey.doctype.goods_at_site.goods_at_site.process_return_items",
-
 					args: {
 						doc: frm.doc,
-						'doc_status': 0
+						doc_status: 0
 					},
 					callback: function (response) {
 						if (response.message) {
-							frm.set_value('custom_status', 'Returned');
-							frappe.msgprint("Successfully created material receipt for return items (Submitted): " + response.message);
-							// Add response.message to child table 'Return Item'
-
-							frm.reload_doc();
+							// Set status and save the form after the status is set
+							frm.set_value('status', 'Returned').then(() => {
+								return frm.save();
+							}).then(() => {
+								frappe.msgprint("Successfully created material receipt for return items (Submitted): " + response.message);
+							});
 						}
 					}
 				});
+
 				// Hide the Return Draft button after click
 				frm.remove_custom_button(__("Return Draft"));
 			}, __("Create"));
 
 			// Add the second button (Return Submit)
 			frm.add_custom_button(__('Return Submit'), function () {
-
-				// frappe.call('ping')
-				// .then(r => {
-				// 	console.log(r)
-				// 	// {message: "pong"}
-				// })
 				frappe.call({
 					method: "iwapp_survey.iwapp_survey.doctype.goods_at_site.goods_at_site.process_return_items",
 					args: {
 						doc: frm.doc,
-						"doc_status": 1
+						doc_status: 1
 					},
 					callback: function (response) {
 						if (response.message) {
-							frm.set_value('custom_status', 'Completed');		
-							frappe.msgprint("Successfully created material receipt for return items (Submitted): " + response.message);
-							// Add response.message to child table 'Return Item'
-
-							frm.reload_doc();
+							// Set status and save the form after the status is set
+							frm.set_value('status', 'Completed').then(() => {
+								return frm.save();
+							}).then(() => {
+								frappe.msgprint("Successfully created material receipt for return items (Submitted): " + response.message);
+							});
 						}
 					}
 				});
+
 				// Hide the Return Submit button after click
 				frm.remove_custom_button(__('Return Submit'));
 			}, __("Create"));
@@ -60,27 +55,25 @@ frappe.ui.form.on('Goods at Site', {
 			frm.custom_buttons_added = true;
 		}
 	},
-	update_indicators(frm) {
-		const indicator = frappe.get_indicator(frm.doc);
-		// if (indicator) {
-		// 	frm.page.set_indicator(indicator[0], indicator[1]);
-		// } else {
-		// 	frm.page.clear_indicator();
-		// }
-		if (frm.doc.status === "Installed") {
-			frm.page.set_indicator("Installed", "grey");
+	update_indicators: function (frm) {
+		const status = frm.doc.status;
+
+		switch (status) {
+			case "Installed":
+				frm.page.set_indicator("Installed", "grey");
+				break;
+			case "Returned":
+				frm.page.set_indicator("Returned", "orange");
+				break;
+			case "Delivered":
+				frm.page.set_indicator("Delivered", "red");
+				break;
+			case "Completed":
+				frm.page.set_indicator("Completed", "blue");
+				break;
+			default:
+				frm.page.clear_indicator();
+				break;
 		}
-		else if (frm.doc.status === "Returned") {
-			frm.page.set_indicator("Returned", "orange");
-		}
-		else if (frm.doc.status === "Delivered") {
-			frm.page.set_indicator("Delivered", "red");
-		}
-		else if (frm.doc.status === "Completed") {
-			frm.page.set_indicator("Completed", "blue");
-		}
-		 else {
-			frm.page.clear_indicator();
-		}
-	},
+	}
 });
